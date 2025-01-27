56b7def809fe344303dbbeb9ece4dff8d8c4a190
Jonathan Lyria <JL154125@hrsfc.ac.uk>
2025-01-27 11:13:49 +0000

    heavily restrctured so it can run under one solution

---
+++ expander/expander/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.IO;
+
+namespace NettspendToSautiSol
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var databasePath =
+               @"C:\Users\jl154125\Source\Repos\jonathanlyria\NettspendToSautiSol\expander\expander\database.db";
+            DatabaseManager databaseManager = new DatabaseManager(databasePath);
+            ArtistExpander artistExpander = new(databaseManager);
+        }
+    }
+}
