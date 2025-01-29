eaf5d04faa1665d6f82cc244fc6ac9ff6a79de6f
jonathanlyria <jon.lyria@gmail.com>
2025-01-29 17:15:38 +0000

    removed unnecesary solutions

--- expander/expander/Program.cs
+++ expander/expander/Program.cs
@@ -8,7 +8,7 @@ namespace NettspendToSautiSol
         public static async Task Main(string[] args)
         {
             var databasePath =
-               @"C:\Users\jl154125\source\repos\jonathanlyria\NettspendToSautiSol\database.db";
+               @"/Users/jonathanlyria/RiderProjects/NettspendToSautiSol/expander/expander/database.db";
             DatabaseManager databaseManager = new DatabaseManager(databasePath);
             ArtistExpander artistExpander = new(databaseManager);
         }
